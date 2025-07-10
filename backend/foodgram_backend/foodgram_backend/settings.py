b4aef9cec03ea4fca890a2040f7663875887bb0e
noupsange <Fedoseevmvf@yandex.ru>
2025-07-10 04:43:14 +0300

    next step in actions

--- backend/foodgram_backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/foodgram_backend/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
 
-DEBUG = os.getenv('DEBUG', default='True').lower() in ('true', '1')
+DEBUG = os.getenv('DEBUG', 'True').lower() in ('true', '1')
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
